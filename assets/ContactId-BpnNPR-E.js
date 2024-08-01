753b2c5db427ed03e9a8383f649f7787c3f6922e
DavydiukDmytro <DavydiukDmytro@users.noreply.github.com>
2024-08-01 11:26:07 +0000

    deploy: c90e9ea4f65ce424720d4da24573db8e7c40d628

---
+++ assets/ContactId-BpnNPR-E.js
@@ -0,0 +1,1 @@
+import{u as a,j as t}from"./index-Bzc0RmcE.js";const o=()=>{const{contactId:s}=a();return t.jsxs(t.Fragment,{children:["ContactId: ",s]})};export{o as default};
--- assets/ContactId-DuzjtUg4.js
+++
@@ -1,1 +0,0 @@
-import{u as a,j as t}from"./index-B0V9d-tC.js";const o=()=>{const{contactId:s}=a();return t.jsxs(t.Fragment,{children:["ContactId: ",s]})};export{o as default};
