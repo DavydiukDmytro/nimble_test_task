96906284ef14ae9387b6fd2b4975cd1761b57f3f
DavydiukDmytro <davudyuck0501@gmail.com>
2024-08-01 17:16:11 +0300

    add redux store

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import './utils/index.css';
 import { App } from './component/App';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-	<React.StrictMode>
+	<Provider store={store}>
 		<BrowserRouter basename='/nimble_test_task'>
 			<App />
 		</BrowserRouter>
-	</React.StrictMode>
+	</Provider>
 );
--- src/page/Contacts/Contacts.jsx
+++ src/page/Contacts/Contacts.jsx
@@ -1,5 +1,40 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { selectError, selectStatusLoading } from '../../store/selectors';
+import { useEffect } from 'react';
+import { getContactsThunk } from '../../store/contacts/thunk';
+import { Modal } from '../../component/Modal';
+import { Loader } from '../../component/Loader';
+
 const Contacts = () => {
-	return <>Contacts</>;
+	const dispatch = useDispatch();
+	const isLoading = useSelector(selectStatusLoading);
+	const error = useSelector(selectError);
+
+	useEffect(() => {
+		dispatch(getContactsThunk());
+	}, [dispatch]);
+
+	return (
+		<div>
+			<section>
+				<h2>Create Contacts</h2>
+			</section>
+			<section>
+				<h2>Contacts</h2>
+			</section>
+			{isLoading && (
+				<Modal>
+					<Loader />
+				</Modal>
+			)}
+			{error && (
+				<Modal>
+					<p>Oops, something went wrong, try again later.</p>
+					<p>Error: {error}</p>
+				</Modal>
+			)}
+		</div>
+	);
 };
 
 export default Contacts;
